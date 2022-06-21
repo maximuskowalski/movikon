dc80f829797374c09ae9c8b0667f9d07a30d85ed
Max Kowalski <max@maxkow.com>
2022-06-21 17:38:16 +1000

    internet down

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ObjectId } = require('mongodb')
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+let db,
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = 'sample_mflix',
+  collection
+
+MongoClient.connect(dbConnectionStr).then(client => {
+  console.log(`Connected to database`)
+  db = client.db(dbName)
+  collection = db.collection('movies')
+})
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
